8e4820146a3d60e64c0f2572545866c22bb344f9
Makeavely196 <115353265+Makeavely196@users.noreply.github.com>
2024-11-12 15:26:43 +0300

    Update bot_my_py.py

--- bot_my_py.py
+++ bot_my_py.py
@@ -2,24 +2,36 @@ import telebot
 from config import botkey
 from datetime import datetime
 import pytz
+from telebot import types
 
 bot = telebot.TeleBot(botkey)
 
+# Функция для создания клавиатуры
+def create_keyboard():
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button_help = types.KeyboardButton('Help')
+    button_time = types.KeyboardButton('Time')
+    markup.add(button_help, button_time)
+    return markup
+
 @bot.message_handler(commands=['start'])
-def handle_message(message):
+def handle_start(message):
     user_name = message.from_user.first_name  # Получаем имя пользователя
-    bot.send_message(message.chat.id, f'Привет, {user_name}❗️')  # Приветствие с именем пользователя
+    bot.send_message(message.chat.id, f'Привет, {user_name}❗️', reply_markup=create_keyboard())
+
+@bot.message_handler(content_types=['text'])
+def handle_text(message):
+    if message.text == 'Help':
+        bot.send_message(message.chat.id, '😀 Я - тестовый бот❗️', reply_markup=create_keyboard())
+    elif message.text == 'Time':
+        # Устанавливаем часовой пояс Москвы
+        moscow_tz = pytz.timezone('Europe/Moscow')
+        # Получаем текущее время в Москве
+        moscow_time = datetime.now(moscow_tz).strftime('%H:%M:%S')
+        bot.send_message(message.chat.id, f'Текущее время в Москве: {moscow_time}', reply_markup=create_keyboard())
 
 @bot.message_handler(commands=['help'])
-def handle_message(message):
-    bot.send_message(message.chat.id, '😀 Я - тестовый бот❗️')
-
-@bot.message_handler(commands=['time'])
-def send_time(message):
-    # Устанавливаем часовой пояс Москвы
-    moscow_tz = pytz.timezone('Europe/Moscow')
-    # Получаем текущее время в Москве
-    moscow_time = datetime.now(moscow_tz).strftime('%H:%M:%S')
-    bot.send_message(message.chat.id, f'Текущее время в Москве: {moscow_time}')
+def handle_help(message):
+    bot.send_message(message.chat.id, '😀 Я - тестовый бот❗️', reply_markup=create_keyboard())
 
 bot.polling()
