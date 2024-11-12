c3c183f70cd730c30230f71d0fecac0ed223627a
Makeavely196 <115353265+Makeavely196@users.noreply.github.com>
2024-11-12 17:52:31 +0300

    Update bot_my_py.py

--- bot_my_py.py
+++ bot_my_py.py
@@ -13,31 +13,38 @@ deck = [f"{rank}{suit}" for suit in suits for rank in ranks]
 # Состояние игры
 players = {}
 game_in_progress = False
+community_cards = []
+pot = 0  # банк для ставок
 
 # Функция для создания клавиатуры
-def create_keyboard():
+def create_keyboard(start_game=False):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    button_play = types.KeyboardButton('Играть в покер')  # Кнопка для начала игры
-    button_end = types.KeyboardButton('Завершить игру')   # Кнопка для завершения игры
-    markup.add(button_play, button_end)
+    if start_game:
+        button_ready = types.KeyboardButton('Начать раздачу')  # Кнопка для начала раздачи карт
+        markup.add(button_ready)
+    else:
+        button_play = types.KeyboardButton('Играть в покер')  # Кнопка для начала игры
+        button_end = types.KeyboardButton('Завершить игру')   # Кнопка для завершения игры
+        markup.add(button_play, button_end)
     return markup
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-    # Отправляем приветственное сообщение и клавиатуру
     bot.send_message(message.chat.id, "Добро пожаловать! Нажмите 'Играть в покер', чтобы начать игру или 'Завершить игру', чтобы выйти.", reply_markup=create_keyboard())
 
 @bot.message_handler(func=lambda message: message.text == 'Играть в покер')
 def start_game(message):
-    global game_in_progress, players, deck
+    global game_in_progress, players, deck, community_cards, pot
     if game_in_progress:
         bot.send_message(message.chat.id, "Игра уже идет! Чтобы начать новую игру, сначала завершите текущую.")
     else:
         game_in_progress = True
         players = {}  # Сбросим список игроков
-        deck = [f"{rank}{suit}" for suit in suits for rank in ranks]  # Перемешиваем колоду
+        deck = [f"{rank}{suit}" for suit in suits for rank in ranks]
         random.shuffle(deck)
-        bot.send_message(message.chat.id, "Игра началась! Введите /join, чтобы присоединиться. Когда все игроки будут готовы, используйте /ready для начала раздачи карт.")
+        community_cards = []  # Сбросим карты на столе
+        pot = 0  # Сбросим банк
+        bot.send_message(message.chat.id, "Игра началась! Введите /join, чтобы присоединиться. Когда все будут готовы, нажмите 'Начать раздачу'.", reply_markup=create_keyboard(start_game=True))
 
 @bot.message_handler(commands=['join'])
 def join_game(message):
@@ -48,7 +55,7 @@ def join_game(message):
     if message.from_user.id in players:
         bot.send_message(message.chat.id, f"{message.from_user.first_name}, вы уже присоединились!")
     else:
-        players[message.from_user.id] = {"name": message.from_user.first_name, "hand": []}
+        players[message.from_user.id] = {"name": message.from_user.first_name, "hand": [], "bet": 0}
         bot.send_message(message.chat.id, f"{message.from_user.first_name} присоединился к игре!")
         notify_players_joined()
 
@@ -56,9 +63,9 @@ def notify_players_joined():
     player_names = [info["name"] for info in players.values()]
     bot.send_message(list(players.keys())[0], f"Игроки, присоединившиеся к игре: {', '.join(player_names)}")
 
-@bot.message_handler(commands=['ready'])
+@bot.message_handler(func=lambda message: message.text == 'Начать раздачу')
 def start_deal(message):
-    global players, game_in_progress
+    global players, game_in_progress, community_cards
     if not game_in_progress:
         bot.send_message(message.chat.id, "Сначала начните игру, нажав 'Играть в покер'.")
         return
@@ -71,17 +78,54 @@ def start_deal(message):
         player_info["hand"] = [deck.pop(), deck.pop()]
         hand = ", ".join(player_info["hand"])
         bot.send_message(player_id, f"Ваши карты: {hand}")
-    bot.send_message(message.chat.id, "Карты разданы! Для завершения игры нажмите 'Завершить игру'.")
+    
+    # Начинаем первый раунд - выкладываем флоп
+    community_cards = [deck.pop() for _ in range(3)]
+    bot.send_message(message.chat.id, f"Флоп: {', '.join(community_cards)}")
+    bot.send_message(message.chat.id, "Сделайте ваши ставки! Используйте команду /bet <сумма>.")
 
-@bot.message_handler(func=lambda message: message.text == 'Завершить игру')
-def end_game(message):
-    global game_in_progress, players
+@bot.message_handler(commands=['bet'])
+def place_bet(message):
+    global pot
     if not game_in_progress:
-        bot.send_message(message.chat.id, "Игра не начата. Нажмите 'Играть в покер' для начала новой игры.")
+        bot.send_message(message.chat.id, "Сначала начните игру, нажав 'Играть в покер'.")
+        return
+
+    try:
+        bet_amount = int(message.text.split()[1])
+    except (IndexError, ValueError):
+        bot.send_message(message.chat.id, "Введите ставку в формате /bet <сумма>.")
+        return
+
+    if bet_amount <= 0:
+        bot.send_message(message.chat.id, "Ставка должна быть больше нуля.")
+        return
+
+    player = players.get(message.from_user.id)
+    if player:
+        player['bet'] = bet_amount
+        pot += bet_amount
+        bot.send_message(message.chat.id, f"{player['name']} поставил {bet_amount}. Общий банк: {pot}")
     else:
-        game_in_progress = False
-        players = {}
-        bot.send_message(message.chat.id, "Игра завершена! Нажмите 'Играть в покер' для новой игры.", reply_markup=create_keyboard())
+        bot.send_message(message.chat.id, "Сначала присоединитесь к игре, используя команду /join.")
+
+@bot.message_handler(commands=['next'])
+def next_round(message):
+    global community_cards, deck
+    if not game_in_progress:
+        bot.send_message(message.chat.id, "Сначала начните игру, нажав 'Играть в покер'.")
+        return
+    if len(community_cards) == 3:
+        # Выкладываем терн
+        community_cards.append(deck.pop())
+        bot.send_message(message.chat.id, f"Тёрн: {', '.join(community_cards)}")
+    elif len(community_cards) == 4:
+        # Выкладываем ривер
+        community_cards.append(deck.pop())
+        bot.send_message(message.chat.id, f"Ривер: {', '.join(community_cards)}")
+        bot.send_message(message.chat.id, "Игра завершена! Используйте команду /showdown для показа карт.")
+    else:
+        bot.send_message(message.chat.id, "Карты на столе уже выложены.")
 
 @bot.message_handler(commands=['showdown'])
 def showdown(message):
@@ -94,7 +138,17 @@ def showdown(message):
     for player_info in players.values():
         hand = ", ".join(player_info["hand"])
         results += f"{player_info['name']}: {hand}\n"
+    results += f"Общие карты: {', '.join(community_cards)}"
     bot.send_message(message.chat.id, results)
     end_game(message)
+
+@bot.message_handler(func=lambda message: message.text == 'Завершить игру')
+def end_game(message):
+    global game_in_progress, players, community_cards, pot
+    game_in_progress = False
+    players = {}
+    community_cards = []
+    pot = 0
+    bot.send_message(message.chat.id, "Игра завершена! Нажмите 'Играть в покер' для новой игры.", reply_markup=create_keyboard())
 
 bot.polling()
