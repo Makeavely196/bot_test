05cb134b31885c97849b5356a17a51759c4704b2
Makeavely196 <115353265+Makeavely196@users.noreply.github.com>
2024-11-12 15:18:25 +0300

    Update bot_my_py.py

--- bot_my_py.py
+++ bot_my_py.py
@@ -1,10 +1,25 @@
 import telebot
 from config import botkey
+from datetime import datetime
+import pytz
+
 bot = telebot.TeleBot(botkey)
+
 @bot.message_handler(commands=['start'])
 def handle_message(message):
-    bot.send_message(message.chat.id, 'Привет❗️')
+    user_name = message.from_user.first_name  # Получаем имя пользователя
+    bot.send_message(message.chat.id, f'Привет, {user_name}❗️')  # Приветствие с именем пользователя
+
 @bot.message_handler(commands=['help'])
 def handle_message(message):
     bot.send_message(message.chat.id, '😀 Я - тестовый бот❗️')
+
+@bot.message_handler(commands=['time'])
+def send_time(message):
+    # Устанавливаем часовой пояс Москвы
+    moscow_tz = pytz.timezone('Europe/Moscow')
+    # Получаем текущее время в Москве
+    moscow_time = datetime.now(moscow_tz).strftime('%H:%M:%S')
+    bot.send_message(message.chat.id, f'Текущее время в Москве: {moscow_time}')
+
 bot.polling()
