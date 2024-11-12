424400b02abcb51d3f70cc25aa0335222391f207
Makeavely196 <115353265+Makeavely196@users.noreply.github.com>
2024-11-12 17:02:56 +0300

    Update bot_my_py.py

--- bot_my_py.py
+++ bot_my_py.py
@@ -8,19 +8,21 @@ bot = telebot.TeleBot(botkey)
 
 # Функция для создания клавиатуры
 def create_keyboard():
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    button_help = types.KeyboardButton('Help')
-    button_time = types.KeyboardButton('Time')
-    markup.add(button_help, button_time)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)  # Создание клавиатуры
+    button_help = types.KeyboardButton('Help')  # Кнопка "Help"
+    button_time = types.KeyboardButton('Time')  # Кнопка "Time"
+    markup.add(button_help, button_time)  # Добавляем кнопки на клавиатуру
     return markup
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
     user_name = message.from_user.first_name  # Получаем имя пользователя
+    # Отправляем сообщение с клавиатурой
     bot.send_message(message.chat.id, f'Привет, {user_name}❗️', reply_markup=create_keyboard())
 
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
+    # Обрабатываем нажатие на кнопки
     if message.text == 'Help':
         bot.send_message(message.chat.id, '😀 Я - тестовый бот❗️', reply_markup=create_keyboard())
     elif message.text == 'Time':
